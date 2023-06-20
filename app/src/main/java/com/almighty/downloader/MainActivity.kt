88dd9d6803789c85138c6752e7f6526791f0ce8d
gaozhongkui <gaozhongkui@meetstarinc.com>
2023-06-20 19:54:41 +0800

    引入：org.jsoup:jsoup:1.16.1

--- app/src/main/java/com/almighty/downloader/MainActivity.kt
+++ app/src/main/java/com/almighty/downloader/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.almighty.downloader
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.almighty.downloader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,21 +13,7 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Example of a call to a native method
-        binding.sampleText.text = stringFromJNI()
     }
 
-    /**
-     * A native method that is implemented by the 'downloader' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
 
-    companion object {
-        // Used to load the 'downloader' library on application startup.
-        init {
-            System.loadLibrary("downloader")
-        }
-    }
 }
