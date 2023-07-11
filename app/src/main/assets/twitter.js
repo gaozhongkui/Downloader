8b14f57c12d228178636dd83bb26fdfa730a7659
gaozhongkui <gaozhongkui@meetstarinc.com>
2023-07-11 19:18:23 +0800

    修改：遍历所有的类型（style）

--- app/src/main/assets/twitter.js
+++ app/src/main/assets/twitter.js
@@ -1,22 +1,27 @@
 var startPrefix = "<div onclick=\"zy()\"";
 var z = startPrefix + "  style='float: right; margin-right:20px'> <img src='https://img1.baidu.com/it/u=646039420,823209841&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500' width=40 height=40/></div>";
-var index = 0;
+var styleArray = ['rec-item rn-typeNewsOne', 'rec-item rn-newsThree', 'rec-item rn-typeSmallVideo', 'rec-item rn-typeNewsOne', 'rec-item rn-typeLargeVideo'];
 
 function zy() {
 	Android.onClicked()
 }
 
 function zz() {
-	var btns = document.getElementsByClassName('rec-item rn-typeNewsOne');
-	while (index < btns.length) {
-		var content = btns[index].innerHTML;
+	styleArray.forEach((item, index, array) => {
+		var index = 0;
+		var btns = document.getElementsByClassName(item);
+		while (index < btns.length) {
+			var content = btns[index].innerHTML;
 
-		if (content.indexOf(startPrefix) < 0) {
-			btns[index].innerHTML += z;
+			if (content.indexOf(startPrefix) < 0) {
+				btns[index].innerHTML += z;
+			}
+
+			index++;
 		}
+	});
 
-		index++;
-	}
+
 }
 
 
