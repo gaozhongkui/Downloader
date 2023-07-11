2773bcfcb7cade4f1b2652ff58699628cab9a6d4
gaozhongkui <gaozhongkui@meetstarinc.com>
2023-07-11 19:08:14 +0800

    修改：如果已经有了，则直接返回

--- app/src/main/assets/twitter.js
+++ app/src/main/assets/twitter.js
@@ -1,4 +1,4 @@
-var startPrefix = "<div onclick=\"zy()\""
+var startPrefix = "<div onclick=\"zy()\"";
 var z = startPrefix + "  style='float: right; margin-right:20px'> <img src='https://img1.baidu.com/it/u=646039420,823209841&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500' width=40 height=40/></div>";
 var index = 0;
 
@@ -9,7 +9,7 @@ function zy() {
 function zz() {
 	var btns = document.getElementsByClassName('rec-item rn-typeNewsOne');
 	while (index < btns.length) {
-		var content = btns[index]
+		var content = btns[index].innerHTML;
 
 		if (content.indexOf(startPrefix) < 0) {
 			btns[index].innerHTML += z;
